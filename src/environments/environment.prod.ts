7f7048d0bf525d170d0521aa1a16b2c027b2bda3
codeTopo <Jose@GWNC31514>
2024-12-12 23:56:57 -0600

    12-12

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,   // Determina si es un entorno de desarrollo o producción
-  apiUrl: 'http://localhost:100/api',// URL base para las solicitudes HTTP en desarrollo
+  apiUrl: 'https://ApiBlancos.somee.com',// URL base para las solicitudes HTTP en desarrollo
   apiKeyCp: '7b65f2b63c31d8f06709349c1f9d2092af6cfe48',
   telefonoBot:3951029107,
   imagentoolbar: 'https://i.ibb.co/BsmGhbf/Blancos-logan.jpg'
