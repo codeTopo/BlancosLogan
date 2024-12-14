7624864feb1994e82b56b10a34089c28b5aab3eb
codeTopo <Jose@GWNC31514>
2024-12-13 19:29:35 -0600

    7:30

--- src/app/cliente/codipo-postal.service.ts
+++ src/app/cliente/codipo-postal.service.ts
@@ -9,28 +9,13 @@ import { environment } from '../../environments/environment.prod';
 })
 export class CodipoPostalService {
 
-  private getHttpOptions() {
-    let token = '';
-    if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-      token = localStorage.getItem('Token') || '';
-    }
-    return {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-
-      })
-    };
-  };
-
   constructor(private http: HttpClient) { }
 
   getCP(cp: string): Observable<CpResponse> {
     if (!/^\d{5}$/.test(cp)) {
       throw new Error('El código postal tiene que tener 5 caracteres numéricos');
     }
-    // URL directa hacia el servicio
-    const apiUrl = `https://api.tau.com.mx/dipomex/v1/codigo_postal?cp=${cp}`;
+    const apiUrl = `/api?cp=${cp}`;
     return this.http.get<CpResponse>(apiUrl, {
       headers: {
         'APIKEY': environment.apiKeyCp
